d93eba63de1dec27808cf396e27f6f5af70c3187
Sandra Carreras Masanet <scm86@alu.ua.es>
2020-02-28 17:03:29 +0100

    Creado caso de prueba unitario para la clase Coche.

---
+++ tests/Unit/CocheTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Coche;
+use App\Conductor;
+use Illuminate\Database\Eloquent\Model;
+//use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+//use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class CocheTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+
+    use DatabaseMigrations;
+
+    public function testRecogerDatos()
+    {
+        Conductor::create(['DNI' => '12345678A', 'Nombre' => 'Antonio', 'Edad' => 20, 'Correo' => 'antonio@dss.com']);
+        Coche::create(['Matricula' => 'A1234BC', 'Marca' => 'Mercedes', 'Modelo' => 'modelo1', 'Pasajeros' => 4, 'Conductor_DNI' => '12345678A']);
+
+        $coche = Coche::query()->first();
+        
+        $this->assertEquals("A1234BC", $coche->Matricula);
+        $this->assertEquals("Mercedes", $coche->Marca);
+        $this->assertEquals('modelo1', $coche->Modelo);
+        $this->assertEquals(4, $coche->Pasajeros);
+        $this->assertEquals('12345678A', $coche->Conductor_DNI);
+
+        //$conductor = Conductor::query()->where('DNI', $coche->Conductor_DNI)->first();
+
+        $conductor = $coche->conductor;
+
+        $this->assertEquals("12345678A", $conductor->DNI);
+        //$this->assertEquals("Antonio", $conductor->Nombre);
+    }
+}
