4eea3773a8c9377ebd1e41a500816286636ad4b6
Sandra Carreras Masanet <scm86@alu.ua.es>
2020-02-28 08:01:26 +0100

    Actualizada clase Coche con atributos rellenables.

--- app/Coche.php
+++ app/Coche.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Coche extends Model
 {
+    protected $fillable = ['Matricula', 'Marca', 'Modelo', 'Pasajeros', 'Conductor_DNI'];
+
     public function conductor() {
         return $this->belongsTo('App\Conductor');
     }
