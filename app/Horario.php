e479ee2b4c804dd50c2ba3d20a4803c1e0b4f646
Sandra Carreras Masanet <scm86@alu.ua.es>
2020-02-28 17:00:35 +0100

    Actualizada clase Horario con atributos rellenables.

--- app/Horario.php
+++ app/Horario.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Horario extends Model
 {
+    protected $fillable = ['Fecha', 'Tipo_viaje', 'Coche_Matricula', 'Hora'];
+
     public function coche(){
         return $this->beongsTo('App\Coche', 'Coche_Matricula', 'Matricula');
     }
