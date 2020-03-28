738ff9143a38a862c9a3aefbc7c59445a144059d
danidobre99 <did2@alu.ua.es>
2020-03-29 00:07:14 +0100

    Modificado tabla linea slots nueva clave ajena a pasajeros

---
+++ database/migrations/2020_03_28_222622_add_pasajero_to_linea_slots_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddPasajeroToLineaSlotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('linea_slots', function (Blueprint $table) {
+            $table->string('Pasajero_id', 9);
+
+            $table->foreign('Pasajero_id')->references('DNI')->on('pasajeros');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('linea_slots', function (Blueprint $table) {
+            $table->dropForeign('linea_slots_Pasajero_id_foreign');
+            
+            $table->dropColumn('Pasajero_id');
+        });
+    }
+}
