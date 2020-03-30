694de2ccc5b009a363ac1f1df57943ad61c9f390
Edgar Martínez Serrano <ems107@alu.ua.es>
2020-03-30 22:51:11 +0200

    Complementado test utilizando la relación hasMany de Pasajero->Linea_Slots y añadida Ruta_id para los coches

--- tests/Unit/PasajeroTest.php
+++ tests/Unit/PasajeroTest.php
@@ -9,6 +9,7 @@ use App\Coche;
 use App\Slot;
 use App\LineaSlot;
 use App\Pasajero;
+use App\Ruta;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 //use Illuminate\Database\QueryException;
 
@@ -24,7 +25,9 @@ class PasajeroTest extends TestCase
     public function testRecogerDatos()
     {
         Conductor::create(['DNI' => '12345678A', 'Nombre' => 'Antonio', 'Edad' => 20, 'Correo' => 'antonio@dss.com']);
-        Coche::create(['Matricula' => 'A1234BC', 'Marca' => 'Mercedes', 'Modelo' => 'modelo1', 'Plazas' => 4, 'Precio' => 1, 'Conductor_DNI' => '12345678A']);
+        Ruta::create(['Localidad' => 'Novelda', 'Universidad' => 'UA']);
+        $ruta_id = Ruta::query()->first()->id;
+        Coche::create(['Matricula' => 'A1234BC', 'Marca' => 'Mercedes', 'Modelo' => 'modelo1', 'Plazas' => 4, 'Precio' => 1, 'Conductor_DNI' => '12345678A', 'Ruta_id' => $ruta_id]);
         Slot::create(['Fecha' => '2020-02-21', 'Tipo_viaje' => 'Ida', 'Coche_Matricula' => 'A1234BC', 'Hora' => '08:15']);  
         Pasajero::create(['DNI' => '11111111E', 'Nombre' => 'Estefania', 'Edad' => 33, 'Correo' => 'estefania@dss.com']);
         
@@ -48,10 +51,13 @@ class PasajeroTest extends TestCase
             }     
         }
 
-        $linea_pasajero = LineaSlot::query()->where('numAsiento', 3)->first()->pasajero()->first();
+        $linea_pasajero = LineaSlot::query()->where('numAsiento', 3)->first()->pasajero;
         $this->assertEquals('11111111E', $linea_pasajero->DNI);
 
         $linea_vacia = LineaSlot::query()->where('numAsiento', 2)->first();
         $this->assertEquals(null, $linea_vacia->Pasajero_id);
+
+        $pasajero_linea = Pasajero::query()->first()->lineaSlots()->first();
+        $this->assertEquals(3, $pasajero_linea->numAsiento);
     }
 }
