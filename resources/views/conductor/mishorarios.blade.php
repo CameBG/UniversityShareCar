80f72e5ef8617b3d9d0cd83c30cc1d624f26b298
danidobre99 <did2@alu.ua.es>
2020-04-03 09:33:35 +0200

    Modificado MisHorarios blade y controller paginacion, estilo y ordenacion asc, desc

--- resources/views/conductor/mishorarios.blade.php
+++ resources/views/conductor/mishorarios.blade.php
@@ -2,13 +2,23 @@
 
 @section('content')
 
-    <table> 
+    <table class = 'table table-striped'> 
         <tr>
-            <th> Fecha </th>
-            <th> Hora </th>
-            <th> Ida/Vuelta </th>
-            <th> Coche </th>
-            <th> Pasajeros </th>
+            <th> 
+                <a href="{{ action('MisHorariosController@showHorarios',  ['sort' => 'fecha', 'sort2' => $sort]) }}">Fecha</a>
+            </th>
+            <th> 
+                <a href="{{ action('MisHorariosController@showHorarios',  ['sort' => 'hora', 'sort2' => $sort]) }}">Hora</a>
+            </th>
+            <th> 
+                <a href="{{ action('MisHorariosController@showHorarios',  ['sort' => 'direccion', 'sort2' => $sort]) }}">Ida/Vuelta</a>
+            </th>
+            <th> 
+                <a href="{{ action('MisHorariosController@showHorarios',  ['sort' => 'nombreCoche', 'sort2' => $sort]) }}">Coche</a>
+            </th>
+            <th> 
+                <a href="{{ action('MisHorariosController@showHorarios',  ['sort' => 'asientos', 'sort2' => $sort]) }}">Pasajeros</a> 
+            </th>
         </tr>
         @foreach ($result as $r)
             <tr>
@@ -21,6 +31,8 @@
         @endforeach
 
     </table>
-    {{$result->links()}}
+    
+    {{$result->appends(['sort' => $sort, 'sort2' => $sort2])->links()}}
+    
 
 @endsection
