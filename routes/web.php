16cf53ebc0881fef0a4097053b35f757851e144a
Sandra Carreras Masanet <scm86@alu.ua.es>
2020-04-03 22:12:24 +0200

    Añadidas rutas a controladores para coches

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,12 @@ Route::get('/', function () {
 Route::get('/pasajeros', 'ConductorController@pasajeros');
 Route::post('/pasajeros', 'ConductorController@pasajeros');
 
+Route::get('/coches', 'ConductorController@coches');
+Route::get('/coches/borrar', 'ConductorController@coches_borrar');
+Route::get('/coches/crear', 'ConductorController@coches_crear');
+Route::post('/coches/crear', 'ConductorController@coches_creado');
+Route::get('/coches/modificar', 'ConductorController@coches_modificar');
+Route::post('/coches/modificar', 'ConductorController@coches_modificado');
+
 Route::get('/mishorarios', 'MisHorariosController@showHorarios');
 Route::post('/mishorarios', 'MisHorariosController@showHorarios');
-
