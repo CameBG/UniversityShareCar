9a4618bccf1329e331c758abd7d4aafd2c6f3a10
Came <cbg69@gcloud.ua.es>
2020-04-03 23:43:45 +0200

    Añadido la funcionalidad de búsqueda por nombre de conductor. Añadidos más seeders para comprobar misreservas

--- database/seeds/LineaSlotsTableSeeder.php
+++ database/seeds/LineaSlotsTableSeeder.php
@@ -29,9 +29,38 @@ class LineaSlotsTableSeeder extends Seeder
             }
         }
         Slot::create(['fecha' => '2010-02-22', 'hora' => '08:15', 'direccion' => 'Vuelta',    'coche_matricula' => 'X5678YZ']);
-        $id_aux = Slot::query()->where('fecha', '2010-02-22')->first()->id;
+        Slot::create(['fecha' => '2015-02-22', 'hora' => '08:15', 'direccion' => 'Vuelta',    'coche_matricula' => 'B1234CD']);
+        Slot::create(['fecha' => '2015-03-22', 'hora' => '08:15', 'direccion' => 'Ida',    'coche_matricula' => 'B1234CD']);
+        Slot::create(['fecha' => '2016-03-22', 'hora' => '08:15', 'direccion' => 'Ida',    'coche_matricula' => 'B1234CD']);
+        Slot::create(['fecha' => '2017-03-22', 'hora' => '08:15', 'direccion' => 'Ida',    'coche_matricula' => 'EP1803H']);
+        Slot::create(['fecha' => '2018-03-22', 'hora' => '08:15', 'direccion' => 'Ida',    'coche_matricula' => 'EP1803H']);
+
+        $id_aux  = Slot::query()->where('fecha', '2010-02-22')->first()->id;
+        $id_aux2 = Slot::query()->where('fecha', '2015-02-22')->first()->id;
+        $id_aux3 = Slot::query()->where('fecha', '2015-03-22')->first()->id;
+        $id_aux4 = Slot::query()->where('fecha', '2016-03-22')->first()->id;
+        $id_aux5 = Slot::query()->where('fecha', '2017-03-22')->first()->id;
+        $id_aux6 = Slot::query()->where('fecha', '2018-03-22')->first()->id;
+
         LineaSlot::create(['slot_id' => $id_aux, 'numAsiento' => 2, 'pasajero_correo' => 'daniel@dss.com']);
         LineaSlot::create(['slot_id' => $id_aux, 'numAsiento' => 1, 'pasajero_correo' => 'daniel@dss.com']);
+
+        
+        LineaSlot::create(['slot_id' => $id_aux2, 'numAsiento' => 1, 'pasajero_correo' => 'alejandra@dss.com']);
+        LineaSlot::create(['slot_id' => $id_aux2, 'numAsiento' => 2, 'pasajero_correo' => 'alejandra@dss.com']);
+
+        LineaSlot::create(['slot_id' => $id_aux3, 'numAsiento' => 1, 'pasajero_correo' => 'alejandra@dss.com']);
+        LineaSlot::create(['slot_id' => $id_aux3, 'numAsiento' => 2, 'pasajero_correo' => 'alejandra@dss.com']);
+
+        LineaSlot::create(['slot_id' => $id_aux4, 'numAsiento' => 1, 'pasajero_correo' => 'alejandra@dss.com']);
+        LineaSlot::create(['slot_id' => $id_aux4, 'numAsiento' => 2, 'pasajero_correo' => 'alejandra@dss.com']);
+
+        LineaSlot::create(['slot_id' => $id_aux5, 'numAsiento' => 2, 'pasajero_correo' => 'alejandra@dss.com']);
+
+        LineaSlot::create(['slot_id' => $id_aux6, 'numAsiento' => 1, 'pasajero_correo' => 'alejandra@dss.com']);
+        LineaSlot::create(['slot_id' => $id_aux6, 'numAsiento' => 2, 'pasajero_correo' => 'alejandra@dss.com']);
+        
+        
         
     }
 }
