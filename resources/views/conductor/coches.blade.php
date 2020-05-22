49ecd2f2b611c410024434d39288fd9683e56cb8
danidobre99 <did2@alu.ua.es>
2020-05-22 22:39:35 +0200

    no va

--- resources/views/conductor/coches.blade.php
+++ resources/views/conductor/coches.blade.php
@@ -15,48 +15,114 @@
         }
     </script>
 
-    <h1> Coches </h1>
 
-    <div>
-        <br><br>
-        <form method="GET" action ="{{ action('ConductorController@coches_crear') }}">
-            @csrf
-            <button type="submit" class="btn btn-primary"><a style="color:white">➕ Añadir nuevo coche</a></button>
-        </form>
+<div class="col-md-5">
+    <div class="card w-100 my-4 mx-5">
+        <div class="card-header">
+            <h1 style="text-align:center; display:inline-block">Coches</h1>
+            <!--<form style="display:inline-block; float:right" method="GET" action ="{{ action('ConductorController@coches_crear') }}">
+                @csrf
+                <button style="margin-right:10px" type="submit" class="btn btn-primary"><i class="fas fa-plus"></i>   Nuevo coche</button>
+            </form>-->
+
+            <button style="display:inline-block; float:right"  type="submit" class="btn btn-primary" data-toggle="modal" data-target="#crearCocheModal"><i class="fas fa-plus"></i>   Nuevo coche</button>
         
-    </div>
-    <div style= "margin-left:33%">
-        <table>
-            @foreach($coches as $coche)
-            <tr height="50px">
-                <td>
-                    {{ $coche->nombreCoche }} &nbsp&nbsp&nbsp&nbsp 
-                    <a onclick="return ConfirmDelete()" href="{{ action('ConductorController@coches_borrar',  ['matricula' => $coche->matricula]) }}"><i style="float:right" class="fas fa-trash-alt">Borrar</i></a>
-                    <a href="{{ action('ConductorController@coches_modificar',  ['matricula' => $coche->matricula, 'page' => $page]) }}"><i style="float:right" class="fas fa-edit">Editar&nbsp&nbsp&nbsp</i></a>
-                </td>
-                <td></td>
-            </tr>
+        </div>
+        <div class="card-body">
+            <table>
+                @foreach($coches as $coche)
+                <tr height="50px">
+                    <td>
+                        <h3 style="display:inline-block">{{ $coche->nombreCoche }} &nbsp&nbsp&nbsp&nbsp </h3>
+                    </td>
+                    <td>
+                        <form style="display:inline-block; float:right" method="POST" action ="{{ action('ConductorController@coches_borrar',  ['matricula' => $coche->matricula]) }}">
+                            @csrf
+                            <button onclick="return ConfirmDelete()" style="margin-right:10px" type="submit" class="btn btn-danger"><i class="fas fa-trash-alt"></i>   Borrar</button>
+                        </form>
 
-            <tr>
-                <td rowspan="5" width="320px" height="20px">
-                    @if (isset($coche->rutaImagen))
-                        <img src="/images/{{ $coche->rutaImagen }}" width="300px" height="auto">
-                    @else
-                        <img src="/defaultImages/defaultCoche.jpg" width="300px" height="auto">
-                    @endif
-                </td>
-                <td>Matricula: {{ $coche->matricula }}</td>
-            </tr>
+                        <button style="margin-right:10px"  type="submit" class="btn btn-primary" data-toggle="modal" data-target="#editar-coche-{{ $coche->matricula }}"><i class="fas fa-plus"></i>   Editar</button>
+                    </td>
+                        
+                        <!--<a onclick="return ConfirmDelete()" href="{{ action('ConductorController@coches_borrar',  ['matricula' => $coche->matricula]) }}"><i style="float:right" class="fas fa-trash-alt">Borrar</i></a>
+                        <a href="{{ action('ConductorController@coches_modificar',  ['matricula' => $coche->matricula, 'page' => $page]) }}"><i style="float:right" class="fas fa-edit">Editar&nbsp&nbsp&nbsp</i></a>-->
+                    </td>
+                    <td></td>
+                </tr>
 
-            <tr><td>Marca: {{ $coche->marca }}</td></tr>
-            <tr><td>Modelo: {{ $coche->modelo }}</td></tr>
-            <tr><td>Plazas: {{ $coche->plazas}}</td></tr>
-            <tr><td>Precio/Viaje: {{ $coche->precioViaje }}€</td></tr>
-            @endforeach
-        </table>
+                <tr>
+                    <td rowspan="5" width="320px" height="20px">
+                        @if (isset($coche->rutaImagen))
+                            <img src="/images/{{ $coche->rutaImagen }}" width="300px" height="auto">
+                        @else
+                            <img src="/defaultImages/defaultCoche.jpg" width="300px" height="auto">
+                        @endif
+                    </td>
+                    <td>Matricula: {{ $coche->matricula }}</td>
+                </tr>
 
+                <tr><td>Marca: {{ $coche->marca }}</td></tr>
+                <tr><td>Modelo: {{ $coche->modelo }}</td></tr>
+                <tr><td>Plazas: {{ $coche->plazas}}</td></tr>
+                <tr><td>Precio/Viaje: {{ $coche->precioViaje }}€</td></tr>
+                @endforeach
+            </table>
+        </div>
         <br>
-
-        {{ $coches->appends([])->links() }}
+        <div class="card-footer">
+            {{ $coches->appends([])->links() }}
+        </div>
     </div>
+</div>
+
+@foreach($coches as $coche)
+<!-- Editar Coche Modal -->
+<div class="modal fade" id="editar-coche-{{ $coche->matricula }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Edita los datos del coche</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+            <form method="POST" action = "{{ action('ConductorController@coches_modificado', ['matricula'=>$coche->matricula, 'page'=>$page]) }}" enctype="multipart/form-data">
+
+                @csrf
+
+                <div class="modal-body"> 
+                    <div class="form-group">
+                        <label for="nombre"> Nombre: </label>
+                        <input type="text" class="form-control" name="nombre" id="nombre" value = "{{$coche->nombreCoche}}" >
+                    </div>    
+                    <div class="form-group">
+                        <label for="fechaNacimiento" > Editar Imagen: </label>
+                        <input name="imagen" id="imagen" type="file" accept="image/jpeg, image/png"/>
+                    </div>  
+                    <div class="form-group">
+                        <label for="nombre" > Marca: </label>
+                        <input type="text" class="form-control" name="marca" id="marca" value = "{{$coche->marca}}">
+                    </div>  
+                    <div class="form-group">
+                        <label for="apellido1" > Modelo: </label>
+                        <input type="text" class="form-control" name="modelo" id="modelo" value = "{{$coche->modelo}}">
+                    </div>        
+                    <div class="form-group">
+                        <label for="apellido2" > Plazas: </label>
+                        <input type="text" class="form-control" name="plazas" id="plazas" value = "{{$coche->plazas}}">
+                    </div> 
+                    <div class="form-group">
+                        <label for="telefono" > Precio/Viaje: </label>
+                        <input type="text" class="form-control" name="telefono" id="telefono" value = "{{$coche->precioViaje}}">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                    <button type="submit" class="btn btn-primary">Guardar Cambios</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+@endforeach
 @endsection
